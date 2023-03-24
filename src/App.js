689221a12abba1d9bd7bd2d98f476ba44a411a0a
ezks <ezequiel31a@gmail.com>
2023-03-24 18:14:46 -0500

    servicios-maez

--- src/App.js
+++ src/App.js
@@ -1,24 +1,209 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  {Header } from './components/common'
+import servicio from './imagenes/soldadura.jpg';
+
+
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html lang="en">
+      <head>
+        <title>Servicios Maez</title>
+      </head>
+      <body>
+       
+       
+        <div id="root">
+         < Header/>
+          <div class="banner">
+            <div class="capa">
+              <div class="contenido">
+                <h1>Estamos dedicados a brindar servicios
+                de confianza para residancias y comercios.</h1>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing<br></br> 
+                elit. Numquam, fugit. Sit architecto modi exercitationem 
+                omnis beatae similique nobis? Veritatis fugiat qui odio</p>
+                <center><a class="button" href='#'>Contactanos!</a></center>
+                </div>
+            </div>
+          </div>
+          <section>
+            <h2>Nuestros Servicios</h2>
+            <div class="servcontainer">
+              <div class="servicios">
+                 <div class="content">
+                   <img class="imgservicio" src={servicio} ></img>
+                   <h3>servicio</h3> <p class="servtext"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, assumenda.</p>
+                </div>
+              </div>
+              <div class="servicios"> 
+                <div class="content">
+                  <img class="imgservicio" src={servicio}></img>
+                  <h3>servicio</h3> 
+                  <p class="servtext"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, assumenda.</p> 
+                </div>
+              </div>
+              <div class="servicios">
+                 <div class="content">
+                  <img class="imgservicio" src={servicio} ></img>
+                  <h3>servicio</h3> 
+                  <p class="servtext"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, assumenda.</p>
+                  </div>
+              </div>
+              <div class="servicios">
+                <div class="content"> 
+                  <img class="imgservicio" src={servicio} ></img>
+                  <h3>servicio</h3>
+                  <p class="servtext"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, assumenda.</p>
+                </div>
+              </div>
+              <div class="servicios">
+                <div class="content">
+                  <img class="imgservicio" src={servicio} ></img>
+                  <h3>servicio</h3>
+                  <p class="servtext"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, assumenda.</p>
+                </div>
+              </div>
+              <div class="servicios">
+                <div class="content"> 
+                  <img class="imgservicio" src={servicio} ></img>
+                  <h3>servicio</h3> 
+                  <p class="servtext"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, assumenda.</p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </div>
+
+
+
+
+
+        <footer>
+          <div class="footer-primerafila">
+              <div class="footer-cuadro-icono">
+                <div class="footer-icono">
+                  
+                </div>
+                <div class="footer-cuadro-texto">
+                  <div class="footer-titulo">
+                    <h5>asesoria</h5>
+                  </div>
+                  <div class="footer-texto">
+                    <p>brindamos asesoria profecional</p>
+                  </div>
+                </div>
+              </div>
+              <div class="footer-cuadro-icono">
+                <div class="footer-icono">
+                </div>
+                <div class="footer-cuadro-texto">
+                  <div class="footer-titulo">
+                    <h5>asesoria</h5>
+                  </div>
+                  <div class="footer-texto">
+                    <p>brindamos asesoria profecional</p>
+                  </div>
+                </div>
+              </div>
+              <div class="footer-cuadro-icono">
+                <div class="footer-icono">
+                </div>
+                <div class="footer-cuadro-texto">
+                  <div class="footer-titulo">
+                    <h5>asesoria</h5>
+                  </div>
+                  <div class="footer-texto">
+                    <p>brindamos asesoria profecional</p>
+                  </div>
+                </div>
+              </div>
+              <div class="footer-cuadro-icono">
+                <div class="footer-icono">
+                </div>
+                <div class="footer-cuadro-texto">
+                  <div class="footer-titulo">
+                    <h5>asesoria</h5>
+                  </div>
+                  <div class="footer-texto">
+                    <p>brindamos asesoria profecional</p>
+                  </div>
+                </div>
+              </div>
+          </div>
+
+          <div class="footer-segundafila">
+              <div class="segundafila-cuadro">
+                <div class="segundafila-titulo">
+                  <h5>asesoria</h5>
+                </div>
+                <div class="segundafila-texto">
+                <p>brindamos asesoria profecional</p>
+                </div>
+              </div>
+              <div class="segundafila-cuadro">
+                <div class="segundafila-titulo">
+                  <h5>asesoria</h5>
+                </div>
+                <div class="segundafila-texto">
+                <p>brindamos asesoria profecional</p>
+                </div>
+              </div>
+              <div class="segundafila-cuadro">
+                <div class="segundafila-titulo">
+                <h5>asesoria</h5>
+                </div>
+                <div class="segundafila-texto">
+                <p>brindamos asesoria profecional</p>
+                </div>  
+              </div>
+              <div class="segundafila-cuadro">
+                <div class="footer-contacto">
+                  <div class="icono-footer-contacto">
+                  
+                  </div>
+                  <div class="texto-footer-contacto">
+                  <p>brindamos asesoria profecional</p>
+                  </div>
+                </div>
+                <div class="footer-contacto">
+                  <div class="icono-footer-contacto">
+                  
+                  </div>
+                  <div class="texto-footer-contacto">
+                  <p>brindamos asesoria profecional</p>
+                  </div>
+                </div>
+                <div class="footer-contacto">
+                  <div class="icono-footer-contacto">
+                  
+                  </div>
+                  <div class="texto-footer-contacto">
+                  <p>brindamos asesoria profecional</p>
+                  </div>
+                  
+                </div>   
+              </div>
+          </div>
+          <div class= "footer-tercerafila">
+              <div class="footer-about">
+
+              </div>
+              <div class="footer-about-espacio">
+                  
+              </div>
+              <div class="footer-redes">
+                  
+              </div>
+              
+          </div>   
+        </footer>
+      </body>
+    </html>
   );
 }
 
---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../../../imagenes/maez.png'
+import  './Header.css'
+function Header(){
+
+return(
+
+<section> 
+<header class="encabezado">
+          <div class="ancho">
+            <div class="logo">
+                 <p><a href='#'> <img class="logo" src={logo}></img> </a></p> 
+              </div>
+              <nav>
+                <ul>
+                  <li><a href='#'>Inicio</a></li>
+                  <li><a href='#'>Nosotros</a></li>
+                  <li><a href='#'>Servicios</a></li>
+                  <li><a href='#'>Contacto</a></li>
+                </ul>
+              </nav>
+          </div>
+        </header>
+</section>)
+    
+}
+export default Header;
